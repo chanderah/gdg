8ef4d340f070836780bb4eb747f9b2044b4518f4
Chandra Sukmagalih Arifin <chandrachansa@gmail.com>
2022-01-03 16:54:32 +0700

    Update tabel_barangmasuk.php

--- application/views/admin/tabel/tabel_barangmasuk.php
+++ application/views/admin/tabel/tabel_barangmasuk.php
@@ -254,7 +254,7 @@
                     <!--<td><?=$dd->terbit?></td></-->
                     <td><a type="button" class="btn btn-info"  href="<?=base_url('admin/update_datamasuk/'.$dd->site_id)?>" name="btn_update" style="margin:auto;"><i class="fa fa-pencil" aria-hidden="true"></i></a></td>
                     <td><a type="button" class="btn btn-danger btn-delete"  href="<?=base_url('admin/delete_data/'.$dd->site_id)?>" name="btn_delete" style="margin:auto;"><i class="fa fa-trash" aria-hidden="true"></i></a></td>
-                    <td><a type="button" class="btn btn-success btn-barangkeluar"  href="<?=base_url('admin/barang_keluar/'.$dd->site_id)?>" name="btn_barangkeluar" style="margin:auto;"><i class="fa fa-sign-out" aria-hidden="true"></i></a></td>
+                    <td><a type="button" class="btn btn-success btn-barangkeluar"  href="<?=base_url('admin/move_data/'.$dd->site_id)?>" name="btn_barangkeluar" style="margin:auto;"><i class="fa fa-sign-out" aria-hidden="true"></i></a></td>
                 </tr>
               <?php $no++; ?>
               <?php endforeach;?>
